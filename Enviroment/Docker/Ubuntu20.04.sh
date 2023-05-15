b8fba57ef71eac50e1076dd7f2ba6f64c2d3d9c5
n132 <xm2146@nyu.edu>
2023-05-15 12:50:32 -0400

    Update Ubuntu20.04.sh

--- Enviroment/Docker/Ubuntu20.04.sh
+++ Enviroment/Docker/Ubuntu20.04.sh
@@ -1,16 +1,11 @@
 # update
 apt update
-apt install vim gdb wget git curl nmap zsh python pip -y
-# remove useless software
-# install ipython
-apt-get install ipython3 -y
+apt install vim ipython3 gdb wget gcc-multilib git curl nmap zsh python pip -y --no-install-recommends
 # install pwntools
-# apt-get install python2.7 python-dev git libssl-dev libffi-dev build-essential -y
 pip3 install pwntools
 # install on-my-zsh   
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-apt-get install gcc-multilib -y
 # install peda pwngdb
 cd ~/
 git clone https://github.com/longld/peda.git ~/peda
